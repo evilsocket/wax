3a072fe6bd9c7ec56dff8314a8eac13de5f629a7
evilsocket <evilsocket@gmail.com>
2018-07-12 22:19:47 +0200

    precomputing bytes classes from the atom dictionary

--- schema/atom.go
+++ schema/atom.go
@@ -1,6 +1,7 @@
 package schema
 
 import (
+	"bytes"
 	"sort"
 )
 
@@ -24,11 +25,16 @@ type Atom struct {
 
 	// TODO: implement custom mutation constraints ( "but" )
 
+	dictionary []byte // list of unique bytes for non scalar types
+	hasSpaces  bool
+	hasSymbols bool
+	hasDigits  bool
+	hasLetters bool
+	hasBinary  bool
 	// this will be updated according to the fitness estimation
 	// pipeline, some differential evaluation magic and eventually
 	// by the feedback loop
-	relevance  float32
-	dictionary []byte // list of unique bytes for non scalar types
+	relevance float32
 }
 
 func (a Atom) IsScalar() bool {
@@ -37,6 +43,7 @@ func (a Atom) IsScalar() bool {
 
 func (a Atom) Prepare(raw []byte) error {
 	if !a.IsScalar() {
+		// build a dictionary of the unique bytes
 		tmp := make(map[byte]bool)
 		for _, b := range raw {
 			tmp[b] = true
@@ -48,6 +55,21 @@ func (a Atom) Prepare(raw []byte) error {
 		}
 
 		sort.Sort(ByteSlice(a.dictionary))
+
+		// check which byte classes are present in the dictionary
+		for _, b := range a.dictionary {
+			if bytes.IndexByte([]byte(" \t\n\v\f\r"), b) != -1 {
+				a.hasSpaces = true
+			} else if (b >= 0x21 && b <= 0x2f) || (b >= 0x3a && b <= 0x40) || (b >= 0x5b && b <= 0x60) || (b >= 0x7b && b <= 0x7e) {
+				a.hasSymbols = true
+			} else if bytes.IndexByte([]byte("1234567890"), b) != -1 {
+				a.hasDigits = true
+			} else if (b >= 0x41 && b <= 0x5a) || (b >= 0x61 && b <= 0x7a) {
+				a.hasLetters = true
+			} else {
+				a.hasBinary = true
+			}
+		}
 	}
 
 	return nil
